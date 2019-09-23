febc62762ad9447041ce2d6611363d1d101de4f7
Técnico em Informática 2018.2 <39121742018.2@senacrjedu.local>
2019-09-23 11:41:19 -0300

    att

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { saveConfig } from '@ionic/core';
+import { Player } from '../model/player';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +14,22 @@ export class PlayerService {
 ) { }
 
 save(player){
-  return this.fire.collection("players").add(player)
+  return this.fire.collection("players")
+  .add({
+    nome: player.nome,
+    nickname: player.nickname,
+    email: player.email,
+    pws: player.pws,
+    ativo:true
+  });
+  }
+
+  gelAll(){
+    return this.fire.collection("players").snapshotChanges()
+    .pipe(
+      map( dados=> 
+        dados.map(d=> ({ key: d.payload.doc.id, ... d.payload.doc.data() }))
+      )
+    )
   }
 }
